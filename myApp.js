64a6e515aee9e4b5958bcf223a96c911c39aebc6
Adithyan2MS <adithyan2ms@gmail.com>
2023-07-17 18:05:40 +0530

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -1,19 +1,22 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
 absolutePath = __dirname + '/views/index.html'
 app.use("/public",express.static(__dirname + '/public'))
 
-const obj={
-    "message":"Hello json"
-}
 
 app.get("/",(req,res)=>{
     res.sendFile(absolutePath)
 })
 
 app.get("/json",(req,res)=>{
-    res.json(obj)
+    console.log(process.env.MESSAGE_STYLE);
+    if (process.env.MESSAGE_STYLE == "uppercase") {
+        res.json({"message":"Hello JSON"})  
+    } else {
+        res.json({"message":"Hello json"})  
+    }
 })
 
 
