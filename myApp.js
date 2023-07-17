1226268a0a07c304a3aa39cdcc06e0a48c7b1496
Adithyan2MS <adithyan2ms@gmail.com>
2023-07-17 18:08:51 +0530

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -13,7 +13,7 @@ app.get("/",(req,res)=>{
 app.get("/json",(req,res)=>{
     console.log(process.env.MESSAGE_STYLE);
     if (process.env.MESSAGE_STYLE == "uppercase") {
-        res.json({"message":"Hello JSON"})  
+        res.json({"message":"HELLO JSON"})  
     } else {
         res.json({"message":"Hello json"})  
     }
